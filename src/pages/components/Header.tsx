fa68efe392021c9951263950c0b4faf96d65b146
GiSeong Lee <cj8928@gmail.com>
2023-08-18 17:01:12 +0900

    헤더 마진 변경

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -3,7 +3,7 @@ import { Box, Typography } from "@mui/material"
 const Header = () => {
   const router = useRouter()
   return (
-    <Box sx={{textAlign: 'center'}}>
+    <Box sx={{textAlign: 'center', marginTop: '100px'}}>
       <Typography  variant="h2" sx={{margin: '20px', cursor: 'pointer',width: 'fit-content',marginLeft: 'auto', marginRight: 'auto',}}
         onClick={()=>router.push('/')}>
           Devrary
