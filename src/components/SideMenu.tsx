c92f5d621ea51b3d74b3418e96190d8c2b68abb3
GiSeong Lee <cj8928@gmail.com>
2023-08-24 15:45:14 +0900

    사이드메뉴 버튼 클릭시 어떤 버튼인지 확인

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,20 +1,30 @@
 import {Box, List, ListItemButton, ListItemText} from '@mui/material'
-const SideMenu = () => {
 
+interface SideMenuProps {
+  setType: (type: string) => void;
+}
+
+
+const SideMenu = ({ setType }: SideMenuProps) => {
+
+  const handleSideBtn = (e: React.MouseEvent<HTMLDivElement>) => {
+    const clickedText = e.currentTarget.innerText;
+    setType(clickedText)
+  }
   return(
     <Box sx={{width: '150px'}}>
       <List>
-        <ListItemButton sx={{textAlign: 'center'}}>
+        <ListItemButton sx={{textAlign: 'center'}} onClick={handleSideBtn}>
           <ListItemText>
             베스트셀러
           </ListItemText>
         </ListItemButton>
-        <ListItemButton sx={{textAlign: 'center'}}>
+        <ListItemButton sx={{textAlign: 'center'}} onClick={handleSideBtn}>
           <ListItemText>
             신간도서
           </ListItemText>
         </ListItemButton>
-        <ListItemButton sx={{textAlign: 'center'}}>
+        <ListItemButton sx={{textAlign: 'center'}} onClick={handleSideBtn}>
           <ListItemText>
             편집자추천
           </ListItemText>
