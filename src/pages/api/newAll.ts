ede9c130466e6650afa4a4d3773332ff661eb1de
GiSeong Lee <cj8928@gmail.com>
2023-08-25 18:34:23 +0900

    신간도서 api생성

---
+++ src/pages/api/newAll.ts
@@ -0,0 +1,31 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+interface NewAllType {
+  title: string;
+  link: string;
+  author: string;
+  publisher: string;
+  pubDate: string;
+  description: string;
+  thumbnailUrl: string;
+  customerReviesRank: number;
+  isbn13: string;
+  priceStandard: number;
+  bestRank: number;
+}
+
+interface NewAllBookType {
+  title: string;
+  pubDate: string;
+  item: NewAllType[];
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<NewAllBookType>
+) {
+  const response = await axios.get('http://101.101.209.241:8080/api/newAll')
+  res.status(200).json(response.data)
+}
